842413702d9327b55b7512c9bda1e5fa8b06635d
gkostoulas <georgekostoulas@gmail.com>
2018-05-18 18:36:55 +0300

    packages changes and css init

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/frontend/js/main.js',
-    mode: 'development',
-    devtool: 'sourcemaps',
-    output: {
-		path: path.resolve(__dirname, './target/classes/static'),
-        filename: 'js/bundle.js'
-    }, 
-     node: {
-        net: 'empty',
-        dns: 'empty'
-      },
-    module: {
+  entry: [
+    './src/main/frontend/js/main.js',
+    './src/main/frontend/scss/main.scss',
+  ],
+  mode: 'development',
+  devtool: 'sourcemaps',
+  output: {
+    path: path.resolve(__dirname, './target/classes/static'),
+    filename: 'js/bundle.js'
+  },
+  node: {
+    net: 'empty',
+    dns: 'empty'
+  },
+  module: {
     rules: [
       {
         test: /\.js$/,
@@ -20,6 +23,20 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
       }
     ]
   }
