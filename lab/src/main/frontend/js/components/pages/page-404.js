5a0005bdc24342e035ce1504f00f57c3872c4895
gkostoulas <georgekostoulas@gmail.com>
2018-08-28 17:55:32 +0300

    Error pages

---
+++ lab/src/main/frontend/js/components/pages/page-404.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+import { StaticRoutes } from '../../model/routes';
+
+export default class Page404 extends React.Component {
+  render() {
+    return (
+      <div className='app flex-row align-items-center'>
+        <Container>
+          <Row className='justify-content-center'>
+            <Col md='6'>
+              <div className='clearfix'>
+                <h1 className='float-left display-3 mr-4'>404</h1>
+                <h4 className='pt-3'>Oops! You're lost.</h4>
+                <p className='text-muted float-left'>The page you are looking for was not found.</p>
+              </div>
+              <NavLink to={StaticRoutes.HOME}>
+                <Button color='primary'><i className='fa fa-home'></i>&nbsp; Take Me Home</Button>
+              </NavLink>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
