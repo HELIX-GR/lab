fcd697f6fe8c7c47735592e902116d69f2924e04
gkostoulas <georgekostoulas@gmail.com>
2018-11-28 20:31:38 +0200

    Links to connected sites

--- lab/src/main/frontend/js/components/views/lab-featured.js
+++ lab/src/main/frontend/js/components/views/lab-featured.js
@@ -100,7 +100,7 @@ class LabFeatured extends React.Component {
               <div className="col-md-3 col-sm-6 col-xs-12" key={key}>
                 <div className="lab-item">
                   <h3 className="lab-title">
-                    <a href="#"> {r.title} </a>
+                    <a href={r.link.ref}> {r.title} </a>
                   </h3>
                   <div className="lab-description">
                     {r.description}
