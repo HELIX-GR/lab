a01a0154b1f6e1aa51a06017155d365e64961163
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-09-18 07:29:39 +0300

    Update pom.xml
    Updated version for all projects to 1.0.0.
    Updated node version to 10.16.3.
    Updated dependencies.
    Moved dependency and plugin versions to properties section.
    Added Shibboleth repository for dependency required by spring-security-saml2-core 1.0.9.RELEASE.
    Updated common submodule.

--- lab/src/main/java/helix/lab/config/HttpSessionConfiguration.java
+++ lab/src/main/java/helix/lab/config/HttpSessionConfiguration.java
@@ -1,16 +1,10 @@
 package helix.lab.config;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.session.MapSessionRepository;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @Profile({ "production", "development" })
 @EnableJdbcHttpSession(tableName = "web.spring_session")
-public class HttpSessionConfig {
-
-    //@Bean
-    public MapSessionRepository sessionRepository() {
-        return new MapSessionRepository();
-    }
+public class HttpSessionConfiguration {
 
 }
