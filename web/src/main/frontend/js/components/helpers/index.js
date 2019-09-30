17be334fd52e6dcf684dfa8e0224b8c147dd3f85
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-09-30 13:40:20 +0300

    Add helper component for role-based rendering

--- web/src/main/frontend/js/components/helpers/index.js
+++ web/src/main/frontend/js/components/helpers/index.js
@@ -1,5 +1,6 @@
 export { default as Checkbox } from './checkbox';
 export { default as Favorite } from './favorite';
 export { default as Pill } from './pill';
+export { default as SecureContent } from './secure-content';
 export { default as SecureRoute } from './secure-route';
 export { default as Tag } from './tag';
---
+++ web/src/main/frontend/js/components/helpers/secure-content.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import * as ReactRedux from 'react-redux';
+
+/**
+ * Renders children only if a role name is provided as
+ * a property and the user has the specific role
+ *
+ * @class SecureContent
+ * @extends {React.Component}
+ */
+class SecureContent extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    roles: PropTypes.arrayOf(PropTypes.string).isRequired,
+    children: PropTypes.element.isRequired,
+  }
+
+  static defaultProps = {
+    roles: [],
+  }
+
+  hasAnyRole(roles) {
+    if ((!roles) || (roles.length === 0)) {
+      return false;
+    }
+
+    const user = this.props.user;
+    if (!user) {
+      return false;
+    }
+
+    for (let role of roles) {
+      if (user.roles.indexOf(role) !== -1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  render() {
+    const { roles } = this.props;
+
+    if (!this.hasAnyRole(roles)) {
+      return null;
+    }
+
+    return this.props.children;
+  }
+}
+
+//
+// Wrap into a connected component
+//
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.profile
+  };
+};
+
+const mapDispatchToProps = null;
+
+SecureContent = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(SecureContent);
+
+export default SecureContent;
