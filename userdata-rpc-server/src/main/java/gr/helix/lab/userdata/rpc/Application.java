e3a4a8c34be1546f154b823e9a9f0ad9cbd2804c
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2019-10-28 02:32:20 +0200

    [userdata-rpc-server] Update configuration example and POM

--- userdata-rpc-server/src/main/java/gr/helix/lab/userdata/rpc/Application.java
+++ userdata-rpc-server/src/main/java/gr/helix/lab/userdata/rpc/Application.java
@@ -2,25 +2,10 @@ package gr.helix.lab.userdata.rpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(
-    scanBasePackageClasses = {
-        gr.helix.core.common.config._Marker.class,
-        gr.helix.core.common.repository._Marker.class,
-        gr.helix.core.common.service._Marker.class,
-        gr.helix.lab.userdata.rpc.config._Marker.class,
-        gr.helix.lab.userdata.rpc.service._Marker.class,
-        gr.helix.lab.userdata.rpc.controller._Marker.class,
-    }
-)
-@EntityScan(
-    basePackageClasses = {
-        gr.helix.core.common.domain._Marker.class,
-    }
-)
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     /**
